ba02cba9b1e32c93a7664c453240569af5a167a6
alexmitroff <alexmitroff@gmail.com>
2019-10-14 14:58:12 +0300

    Add basic project structure

---
+++ docker/kos_django/dev-run.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Check changes in models"
+python manage.py makemigrations --dry-run
+
+python manage.py runserver 0.0.0.0:8000
---
+++ kos_imitator/settings/dev.py
@@ -0,0 +1,6 @@
+from kos_imitator.settings.base import *
+
+SECRET_KEY = '52h#0@ivjh-jl+*)6^z2r6kfym9eqp-z&&ivwwslm@=mou6#sm'
+DEBUG = True
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
